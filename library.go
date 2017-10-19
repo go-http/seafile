d2680733f22aec9239da5e1b79a2d40b4c75e98f
Athurg Feng <athurg@gooth.org>
2017-10-19 16:50:52 +0800

    调整资料库相关API的结构

--- library.go
+++ library.go
@@ -3,7 +3,10 @@ package seafile
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 const (
@@ -15,6 +18,8 @@ const (
 
 //资料库信息
 type Library struct {
+	client *Client
+
 	Encrypted bool
 	Virtual   bool
 	Version   int
@@ -33,32 +38,32 @@ type Library struct {
 }
 
 //获取所有可用资料库列表
-func (cli *Client) ListAllLibraries() ([]Library, error) {
+func (cli *Client) ListAllLibraries() ([]*Library, error) {
 	return cli.ListLibrariesByType("")
 }
 
 //获取我的资料库列表
-func (cli *Client) ListOwnedLibraries() ([]Library, error) {
+func (cli *Client) ListOwnedLibraries() ([]*Library, error) {
 	return cli.ListLibrariesByType(LibraryTypeMine)
 }
 
 //获取私人共享给我的资料库列表
-func (cli *Client) ListSharedLibraries() ([]Library, error) {
+func (cli *Client) ListSharedLibraries() ([]*Library, error) {
 	return cli.ListLibrariesByType(LibraryTypeShared)
 }
 
 //获取群组共享的资料库列表
-func (cli *Client) ListGroupLibraries() ([]Library, error) {
+func (cli *Client) ListGroupLibraries() ([]*Library, error) {
 	return cli.ListLibrariesByType(LibraryTypeGroup)
 }
 
 //获取公共的资料库列表
-func (cli *Client) ListOrgLibraries() ([]Library, error) {
+func (cli *Client) ListOrgLibraries() ([]*Library, error) {
 	return cli.ListLibrariesByType(LibraryTypeOrg)
 }
 
 //获取指定类型的资料库列表
-func (cli *Client) ListLibrariesByType(libType string) ([]Library, error) {
+func (cli *Client) ListLibrariesByType(libType string) ([]*Library, error) {
 	path := "/repos/"
 	if libType != "" {
 		path += "?type=" + libType
@@ -70,18 +75,29 @@ func (cli *Client) ListLibrariesByType(libType string) ([]Library, error) {
 	}
 	defer resp.Body.Close()
 
-	info := []Library{}
+	info := []*Library{}
 	err = json.NewDecoder(resp.Body).Decode(&info)
 	if err != nil {
 		return nil, fmt.Errorf("读取错误:%s %s", resp.Status, err)
 	}
 
+	for _, lib := range info {
+		lib.client = cli
+	}
+
 	return info, nil
 }
 
+func (lib *Library) doRequest(method, uri string, header http.Header, body io.Reader) (*http.Response, error) {
+	if !strings.HasPrefix(uri, "http://") && !strings.HasPrefix(uri, "https://") {
+		uri = "/repos/" + lib.Id + uri
+	}
+	return lib.client.doRequest(method, uri, header, body)
+}
+
 //获取资料库的上传地址
-func (cli *Client) LibraryUploadLink(libId string) (string, error) {
-	resp, err := cli.doRequest("GET", "/repos/"+libId+"/upload-link/", nil, nil)
+func (lib *Library) UploadLink() (string, error) {
+	resp, err := lib.doRequest("GET", "/upload-link/", nil, nil)
 	if err != nil {
 		return "", fmt.Errorf("请求错误:%s", err)
 	}
@@ -97,8 +113,8 @@ func (cli *Client) LibraryUploadLink(libId string) (string, error) {
 }
 
 //获取资料库的更新地址
-func (cli *Client) LibraryUpdateLink(libId string) (string, error) {
-	resp, err := cli.doRequest("GET", "/repos/"+libId+"/update-link/", nil, nil)
+func (lib *Library) UpdateLink() (string, error) {
+	resp, err := lib.doRequest("GET", "/update-link/", nil, nil)
 	if err != nil {
 		return "", fmt.Errorf("请求错误:%s", err)
 	}
