cf097a0637f50d923daf8baf2ece72eaaeedb311
Athurg Feng <athurg@gooth.org>
2017-10-19 10:09:02 +0800

    增加API /ping的支持

--- client.go
+++ client.go
@@ -1,6 +1,9 @@
 package seafile
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 )
 
@@ -14,4 +17,20 @@ const apiPrefix = "/api2"
 func New(hostname string) *Client {
 	hostname = strings.TrimSuffix(hostname, "/")
 	return &Client{Hostname: hostname + apiPrefix}
+}
+
+//测试Seafile服务连通性
+func (cli *Client) Ping() (string, error) {
+	resp, err := http.Get(cli.Hostname + "/ping")
+	if err != nil {
+		return "", fmt.Errorf("请求错误:%s", err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("读取错误:%s", err)
+	}
+
+	return string(b), nil
 }
