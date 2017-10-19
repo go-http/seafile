f3c3854010f6fc820215954492bbc1a308fa457b
Athurg Feng <athurg@gooth.org>
2017-10-19 10:37:11 +0800

    增加API /auth-token/的支持

--- client.go
+++ client.go
@@ -1,14 +1,17 @@
 package seafile
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 type Client struct {
-	Hostname string
+	Hostname  string
+	authToken string
 }
 
 const apiPrefix = "/api2"
@@ -33,4 +36,41 @@ func (cli *Client) Ping() (string, error) {
 	}
 
 	return string(b), nil
+}
+
+//获取AuthToken
+func (cli *Client) AuthToken(username, password string) (string, error) {
+	formData := url.Values{
+		"username": {username},
+		"password": {password},
+	}
+
+	resp, err := http.PostForm(cli.Hostname+"/auth-token/", formData)
+	if err != nil {
+		return "", fmt.Errorf("请求错误:%s", err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("读取错误:%s %s", resp.Status, err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("%s %s", resp.Status, string(b))
+	}
+
+	//TODO: 找到真正的返回结构
+	respInfo := map[string]string{}
+	err = json.Unmarshal(b, &respInfo)
+	if err != nil {
+		return "", fmt.Errorf("读取错误:%s", err)
+	}
+
+	if respInfo["token"] != "" {
+		cli.authToken = respInfo["token"]
+		return cli.authToken, nil
+	} else {
+		return "", fmt.Errorf("%s", string(b))
+	}
 }
--- client_test.go
+++ client_test.go
@@ -9,8 +9,21 @@ func TestPing(t *testing.T) {
 	hostname := os.Getenv("SEAFILE_HOST")
 	result, err := New(hostname).Ping()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	t.Log(result)
+	t.Log("ping返回", result)
+}
+
+func TestAuthToken(t *testing.T) {
+	hostname := os.Getenv("SEAFILE_HOST")
+	username := os.Getenv("SEAFILE_USER")
+	password := os.Getenv("SEAFILE_PASS")
+
+	token, err := New(hostname).AuthToken(username, password)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("获取到Token", token)
 }
