4e84b18e0d7042d2b62be20422ee0c7152ddde7a
Jianbo Feng <fengjianbo@nibirutech.com>
2017-10-19 15:04:00 +0800

    增加资料库上传文件内容的方法

--- library.go
+++ library.go
@@ -1,8 +1,13 @@
 package seafile
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"strings"
 )
 
 const (
@@ -76,4 +81,87 @@ func (cli *Client) ListLibrariesByType(libType string) ([]Library, error) {
 	}
 
 	return info, nil
+}
+
+//获取资料库的上传地址
+func (cli *Client) LibraryUploadLink(libId string) (string, error) {
+	resp, err := cli.doRequest("GET", "/repos/"+libId+"/upload-link/", nil, nil)
+	if err != nil {
+		return "", fmt.Errorf("请求错误:%s", err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("读取错误:%s %s", resp.Status, err)
+	}
+
+	//返回值是"xxx"格式的，需要去掉头尾的引号
+	return string(b[1 : len(b)-1]), nil
+}
+
+//上传文件内容
+//    fileContentMap的key是文件名，value是文件内容
+//当目标文件存在时，会自动重命名上传
+func (cli *Client) UploadFileContent(libId, parentDir string, fileContentMap map[string][]byte) error {
+	//参数检查
+	if !strings.HasSuffix(parentDir, "/") {
+		return fmt.Errorf("目录%s必须以/结尾", parentDir)
+	}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	//填充文件内容
+	for filename, content := range fileContentMap {
+		part, err := writer.CreateFormFile("file", filename)
+		if err != nil {
+			return fmt.Errorf("创建Multipart错误:%s", err)
+		}
+		part.Write(content)
+	}
+
+	//填充其他字段
+	writer.WriteField("parent_dir", parentDir)
+
+	//FIXME:
+	//文档中提到使用relative_path，系统会自动创建不存在的路径
+	//但实际上好像没有效果，所以这里暂不支持这个参数
+	//writer.WriteField("relative_path", subDir)
+
+	err := writer.Close()
+	if err != nil {
+		return fmt.Errorf("写Multipart文件错误:%s", err)
+	}
+
+	//设置请求Header
+	header := http.Header{"Content-Type": {writer.FormDataContentType()}}
+
+	//获取上传地址
+	uploadLink, err := cli.LibraryUploadLink(libId)
+	if err != nil {
+		return fmt.Errorf("获取上传地址错误:%s", err)
+	}
+
+	//执行上传
+	resp, err := cli.doRequest("POST", uploadLink+"?ret-json=1", header, body)
+	if err != nil {
+		return fmt.Errorf("请求错误:%s", err)
+	}
+	defer resp.Body.Close()
+
+	respInfo := []DirectoryEntry{}
+	err = json.NewDecoder(resp.Body).Decode(&respInfo)
+	if err != nil {
+		return fmt.Errorf("解析错误:%s", err)
+	}
+
+	//Debug output
+	fmt.Printf("返回%+v\n", respInfo)
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("返回%s", resp.Status)
+	}
+
+	return nil
 }
