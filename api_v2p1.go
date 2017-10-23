7a5faa5e3fe718cac67807dc0334ce7aab3a179d
Athurg Feng <athurg@gooth.org>
2017-10-23 11:30:48 +0800

    添加POST表单的函数

--- api_v2p1.go
+++ api_v2p1.go
@@ -1,8 +1,10 @@
 package seafile
 
 import (
+	"bytes"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 func (cli *Client) apiRequestV2p1(method, uri string, header http.Header, body io.Reader) (*http.Response, error) {
@@ -11,6 +13,11 @@ func (cli *Client) apiRequestV2p1(method, uri string, header http.Header, body i
 
 func (cli *Client) apiGET(uri string) (*http.Response, error) {
 	return cli.apiRequestV2p1("GET", uri, nil, nil)
+}
+
+func (cli *Client) apiPOSTForm(uri string, form url.Values) (*http.Response, error) {
+	header := http.Header{"Content-Type": {"application/x-www-form-urlencoded"}}
+	return cli.apiRequestV2p1("POST", uri, header, bytes.NewBufferString(form.Encode()))
 }
 
 func (cli *Client) apiPOST(uri string, header http.Header, body io.Reader) (*http.Response, error) {
