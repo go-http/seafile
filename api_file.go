bd580806d6f2cf2d6f2d0f942d5b3d9bc808dd62
Jianbo Feng <fengjianbo@nibirutech.com>
2017-10-23 10:33:57 +0800

    增加创建文件的方法

--- api_file.go
+++ api_file.go
@@ -43,3 +43,40 @@ func (repo *Repo) GetFile(path string) (*File, error) {
 
 	return &file, nil
 }
+
+//检查文件是否存在，如果不存在则创建，返回文件本身
+func (repo *Repo) TouchFile(path string) (*File, error) {
+	file, err := repo.GetFile(path)
+	if err != nil {
+		return file, nil
+	}
+
+	return repo.CreateFile(path)
+}
+
+//创建文件
+//如果文件已存在，则按照重命名规则创建新文件
+//如果不希望创建重命名的文件，建议使用Repo.Touch方法
+func (repo *Repo) CreateFile(path string) (*File, error) {
+	q := url.Values{"p": {path}}
+	d := url.Values{"operation": {"create"}}
+	resp, err := repo.client.apiPOSTForm(repo.Uri()+"/file/?"+q.Encode(), d)
+	if err != nil {
+		return nil, fmt.Errorf("请求资料库信息失败: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("资料库信息错误: %s", resp.Status)
+	}
+
+	var file File
+	err = json.NewDecoder(resp.Body).Decode(&file)
+	if err != nil {
+		return nil, fmt.Errorf("解析资料库信息失败: %s, %s", resp.Status, err)
+	}
+
+	file.repo = repo
+
+	return &file, nil
+}
